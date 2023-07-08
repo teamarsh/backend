f9a55364a5e2bf56f77f601b602a753537a0a915
shtajiryan <shtajiryan@hotmail.com>
2023-07-08 11:30:49 +0400

    test

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,8 @@ COPY package*.json ./
 
 RUN npm install
 
+COPY . /usr/src/app/backend
+
 EXPOSE 3000
 
 CMD ["npm", "run", "start"]
--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const app = express();
 
 
 // view engine setup
-app.set('views', path.join(__dirname, '/view'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(cors({origin:'*'}))
 app.use(bodyParser.urlencoded({extended: true}));
